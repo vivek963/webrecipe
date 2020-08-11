d49448a655f1cc8cdc75c8096ea7c48fa3fed1a1
vivek kamdar <vivek.kamdar3@gmail.com>
2020-08-11 13:01:02 +0100

    changed

--- application/controllers/Restaurants.php
+++ application/controllers/Restaurants.php
@@ -3,31 +3,22 @@
 class Restaurants extends CI_Controller {
 
     function restaurantsFood() {
-        $message = $this->input->get('message');
-        $data = array();
-        $get_fields = 'query=' . $message;
-         $curl = curl_init();
+        $headerValue = array(
+            "X-API-Token: wiWqcp0ZcHXw",
+            "Authorization: Basic cnlhbmRzejIzNDdAZ21haWwuY29tOjEyM0RyYWdvbiQ="
+        );
+        $url = 'http://restro-service.us-east-2.elasticbeanstalk.com/restrosearch/services/api/place/search?name=restaurants&lat=53.3472641&long=-6.226591099999999';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headerValue);
 
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => "http://restro-service.us-east-2.elasticbeanstalk.com/restrosearch/services/api/place/search?name=bar%20italia%20ristorante&lat=53.3472641&long=-6.226591099999999",
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 0,
-      CURLOPT_FOLLOWLOCATION => true,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => array(
-        "X-API-Token: wiWqcp0ZcHXw",
-        "Authorization: Basic cnlhbmRzejIzNDdAZ21haWwuY29tOjEyM0RyYWdvbiQ="
-      ),
-    ));
-    
-    $response = curl_exec($curl);
-    
-    curl_close($curl);
-    echo $response;
+        $res = curl_exec($ch);
+        curl_close($ch);
 
-}
+        print_r($res);
+    }
 
 }
--- application/views/restaurants.php
+++ application/views/restaurants.php
@@ -1,46 +1,89 @@
-    <div class="container-fluid padding">
-        <div class="row welcome text-center">
-            <div class="col-12">
-                <h1 class="display-4">Restaurants</h1>
-    
-            </div>
+<div class="container-fluid padding">
+    <div class="row welcome text-center">
+        <div class="col-12">
+            <h1 class="display-4">Restaurants</h1>
+
+        </div>
         <hr>
         <div class="col-12">
             <p class="lead">Famous Restaurants in Dublin</p>
         </div>
-        </div>
-
-
-        <div class="row" id="restaurantsFood">
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-            
-                        <div class="card mb-4">
-                            <img class="card-img-top" src="" alt="card image cap">
-                            <div class="card-body">
-            
-                                <h2 class="h4 card-title">Name</h2>
-                                <p class="card-text"></p>
-                                <h3 class="h5 card-subtitle mb-3">Rating 4.5/5 (Reviews)</h3>
-                                <h3 class="h5 card-subtitle mb-3">Address:</h3>
-                                <p class="card-text"></p>
-                                <a href="#" class="btn btn-primary">More info</a>
-            
-                            </div>
-                        </div>
-                    </div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/common.js"  type="text/javascript"></script>
-
-<script>
-
-
-
-
-
-</script>
-
-        
     </div>
 
-    
+
+    <div class="row" id="restaurantsFood">
+        <!--        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+        
+                    <div class="card mb-4">
+                        <img class="card-img-top" src="" alt="card image cap">
+                        <div class="card-body">
+        
+                            <h2 class="h4 card-title">Name</h2>
+                            <p class="card-text"></p>
+                            <h3 class="h5 card-subtitle mb-3">Rating 4.5/5 (Reviews)</h3>
+                            <h3 class="h5 card-subtitle mb-3">Address:</h3>
+                            <p class="card-text"></p>
+                            <a href="#" class="btn btn-primary">More info</a>
+        
+                        </div>
+                    </div>
+                </div>-->
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/common.js"  type="text/javascript"></script>
+
+    <script>
+        getRestaurant();
+        function getRestaurant() {
+            let url = "<?php echo base_url(); ?>" + 'Restaurants/restaurantsFood';
+            $.ajax({
+                url: url,
+                crossDomain: true,
+                type: 'GET',
+                dataType: 'json',
+                success: function (res) {
+                    if (res.responseCode == 'OK') {
+                        let data = '';
+                        let image_ref = '';
+                        let image = '';
+                        let status = 'NA';
+                        let status_val = '';
+                        for (let i = 0; i < res.data.results.length; i++) {
+                            image_ref = res.data.results[i].photos[0].photo_reference;
+                            image = 'https://maps.googleapis.com/maps/api/place/photo?&key=AIzaSyAHeTasNwmuENWkUYfkQAhi9-YHTlQwiXA&maxheight=700&maxwidth=700&photoreference=' + image_ref;
+                            if ("opening_hours" in res.data.results[i]) {
+                                status = res.data.results[i].opening_hours['open_now'];
+                                if (status == true) {
+                                    status_val = "OPEN";
+                                } else {
+                                    status_val = "CLOSED";
+                                }
+                            }
+
+                            data += `<div class="col-12 col-md-6 col-lg-4 col-xl-3">
+        <div class="card mb-4">
+            <img class="card-img-top" height="300" src="` + image + `" alt="card image cap">
+            <div class="card-body">
+                <h2 class="h4 card-title">` + res.data.results[i].name + `</h2>
+                <p class="card-text"></p>
+                <h3 class="h5 card-subtitle mb-3">User's Rating: ` + res.data.results[i].rating + `/5</h3>
+                <h3 class="h5 card-subtitle mb-3">Address: ` + res.data.results[i].vicinity + `</h3>
+                <h3 class="h5 card-subtitle mb-3">Status: ` + status_val + `</h3>
+
+            </div>
+        </div>
+    </div>`;
+                        }
+                        $('#restaurantsFood').html(data);
+                    } else {
+                        alert(res.message);
+                    }
+                }
+            });
+        }
+    </script>
+
+
+
+
+
